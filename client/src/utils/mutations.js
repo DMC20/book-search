cff99be06906dd1c7e6b33e617debac07e71232c
Daniel Carazo <danielcarazo20@gmail.com>
2021-10-18 12:48:10 -0400

    completed login form, edited mutation file

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,15 +1,14 @@
 import { gql } from '@apollo/client';
 
-export const LOGIN_USER = gql `
-    mutation login($email: String!, $password: String!) {
-        login(email: $email), password: $password) {
-            token
-            user {
-                _id
-                username
-            }
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+        token
+        user {
+          _id
         }
     }
+}
 `;
 
 export const ADD_USER = gql `
@@ -20,6 +19,13 @@ export const ADD_USER = gql `
                 _id
                 username
                 email
+                bookCount
+                savedBooks {
+                    bookId
+                    authors
+                    desciption
+                    link
+                }
             }
         }
     }
